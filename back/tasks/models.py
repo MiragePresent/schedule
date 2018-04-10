a1652bf09a2a9d5476490fbe8ef4a37cac228755
David Holovii <mirage.present@gmail.com>
2018-04-10 12:17:00 +0300

    Task model

--- back/tasks/models.py
+++ back/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Models):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_lenght = 300)
+    description = models.CharField(max_length = 1000)
+    day = models.DateField()
+    to_time = models.DateTimeField()
+    to_time = models.DateTimeField()
