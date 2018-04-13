84d239addf3d75a53193ed9f5dbc2ecae589320f
David Holovii <mirage.present@gmail.com>
2018-04-13 16:39:01 +0300

    With bugs

---
+++ front/src/app/screens/Schedule/components/TaskListEmpty.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list-tile avatar>
+    <v-list-tile-content>
+      <v-list-tile-title
+        v-text="message"
+        class="text-md-center"
+      />
+    </v-list-tile-content>
+  </v-list-tile>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'You have no tasks',
+    };
+  },
+}
+</script>
+<style>
+</style>
