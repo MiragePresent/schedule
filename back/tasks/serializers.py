ebf52f1b2679ddd9c4451091274242c4c2ffeb30
David Holovii <mirage.present@gmail.com>
2018-04-13 16:44:19 +0300

    Min length title rule

--- back/tasks/serializers.py
+++ back/tasks/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from tasks.models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+
+    def validate_title(self, value):
+         if len(value) < 2:
+            raise serializers.ValidationError("Title must be longer than 2 characters")
+
     class Meta:
         model = Task
         fields = ('id','title', 'status')
